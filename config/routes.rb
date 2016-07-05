6c636bf30efbabb343bb8280c436311820fd9781
vedabrat <vedabrat>
2016-07-05 19:23:39 -0400

    omg i finally know how good backing up is jesus christ that was hard

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages #home'
+  root 'static_pages#home'
 end
