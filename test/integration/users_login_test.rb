6388651ad3eca2429040de6446f6f78fa5d9600c
vedabrat <vedabrat>
2016-07-15 17:34:22 -0400

    omg safepoint at listing 9.31

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -40,4 +40,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+
 end
